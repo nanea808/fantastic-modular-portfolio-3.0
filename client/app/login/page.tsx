3aaa2667b99c5b01384c0b57f4883fe3acfabecb
Ethan Willers <willers.enw@gmail.com>
2023-05-21 22:46:51 -0700

    created login page

---
+++ client/app/login/page.tsx
@@ -0,0 +1,39 @@
+export default function Login() {
+  return (
+    <form className="w-full max-w-sm">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="username"
+          >
+            Username
+          </label>
+        </div>
+        <div className="md:w-2/3"><input className="block bg-slate-700 text-white p-1 rounded-md" id="username" type="text"/></div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="inline-password"
+          >
+            Password
+          </label>
+        </div>
+        <div className="md:w-2/3"><input className="block bg-slate-700 text-white p-1 rounded-md" id="username" type="password"/></div>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button
+            className="shadow bg-blue-400 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
