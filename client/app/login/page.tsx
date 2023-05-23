703ce31baa81cb9cd0afa7e6978e4dc2fb799d91
Ethan Willers <willers.enw@gmail.com>
2023-05-22 22:29:16 -0700

    useState implemented in login input

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useState } from "react";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUsernameChange(e: any) {
+    setUsername(e.target.value);
+  }
+  function handlePasswordChange(e: any) {
+    setPassword(e.target.value);
+  }
+
   return (
     <form className="w-full max-w-sm">
       <div className="md:flex md:items-center mb-6">
@@ -10,7 +23,15 @@ export default function Login() {
             Username
           </label>
         </div>
-        <div className="md:w-2/3"><input className="block bg-slate-700 text-white p-1 rounded-md" id="username" type="text"/></div>
+        <div className="md:w-2/3">
+          <input
+            className="block bg-slate-700 text-white p-1 rounded-md"
+            id="username"
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
       </div>
       <div className="md:flex md:items-center mb-6">
         <div className="md:w-1/3">
@@ -21,16 +42,24 @@ export default function Login() {
             Password
           </label>
         </div>
-        <div className="md:w-2/3"><input className="block bg-slate-700 text-white p-1 rounded-md" id="username" type="password"/></div>
+        <div className="md:w-2/3">
+          <input
+            className="block bg-slate-700 text-white p-1 rounded-md"
+            id="username"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
       </div>
       <div className="md:flex md:items-center">
         <div className="md:w-1/3"></div>
         <div className="md:w-2/3">
           <button
-            className="shadow bg-blue-400 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            className="shadow bg-blue-400 hover:bg-blue-300 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="button"
           >
-            Sign Up
+            Login
           </button>
         </div>
       </div>
