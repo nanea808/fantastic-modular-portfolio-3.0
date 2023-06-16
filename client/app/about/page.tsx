eb41e039a173896af60a984a18b55f48a3c2bd5d
Ethan Willers <willers.enw@gmail.com>
2023-06-15 20:58:35 -0700

    added working login route

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -4,7 +4,11 @@ export default function About() {
   return (
     <div>
       <h1 className="font-bold text-3xl font-serif">About</h1>
-      <p className="my-5 max-w-[460px] font-sans">this is the about page. It is very epic.</p>
+      <p className="my-5 max-w-[460px] font-sans">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati iste
+        ad ipsum id ipsa! Quae, alias impedit praesentium, nobis qui dolorem
+        reprehenderit cumque amet magni voluptatem ab, accusamus nulla ipsum?
+      </p>
     </div>
   );
 }
